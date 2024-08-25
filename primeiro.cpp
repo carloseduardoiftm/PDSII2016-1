9afc0360f9840bc392228f9cc09623ea487bb889
carloseduardoiftm <carloseduardodantas@iftm.edu.br>
2024-08-25 18:24:22 -0300

    programa em c

---
+++ primeiro.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+     
+     int valor;
+     
+     printf("Digite um numero");
+     scanf("&d",valor);
+     
+     printf("&d",valor);
+     
+     return 0;
+    
+
+}
